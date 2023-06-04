d2448251a2794a30b5fded808619604d6214633e
Alexander Kobez <alexander.kobez@gmail.com>
2023-06-04 08:18:24 +0300

    Create Exclusive 'or' (xor) Logical Operator.cpp

---
+++ 8 kyu /Exclusive 'or' (xor) Logical Operator.cpp
@@ -0,0 +1,14 @@
+bool xorf( bool a, bool b ) {
+  // 🐲 --my name is Xor!!
+  return a ^ b;
+  return a - b;
+  return a != b;
+  return a xor b;
+  return a + b & 1;
+  return a ? !b : b;
+  return ( a + b ) % 2;
+  return a & !b || !a & b;
+  return ( a | b ) & ( !a | !b );
+  return a ? b ? 0 : 1 : b ? 1 : 0;
+  // 💃 --I have multilevel protection! 🖕
+}
