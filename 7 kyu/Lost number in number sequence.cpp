e08628c86c8e4ad87220b1d7de787181b558d9ff
Alexander Kobez <alexander.kobez@gmail.com>
2023-07-15 14:16:51 +0300

    Update Lost number in number sequence.cpp

--- 7 kyu/Lost number in number sequence.cpp
+++ 7 kyu/Lost number in number sequence.cpp
@@ -1,4 +1,16 @@
+#define V2
+
+#ifdef V1
 #include <numeric>
 int findDeletedNumber( std::list< int > s, std::list< int > m ) {
   return s.back() * ++s.back() / 2 - accumulate( m.cbegin(), m.cend(), 0 );
 }
+
+#elif defined( V2 )
+#include <numeric>
+int findDeletedNumber( std::list< int > s, std::list< int > m ) {
+  s.splice( s.cend(), m );
+  return accumulate( s.cbegin(), s.cend(), 0, std::bit_xor() );
+}
+
+#endif
