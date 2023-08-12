ac3bcb668e1dae4fe486f08598bbc0bc609ee4b6
Alexander Kobez <alexander.kobez@gmail.com>
2023-08-12 18:33:01 +0300

    Create Baby shark lyrics generator.cpp

---
+++ 7 kyu/Baby shark lyrics generator.cpp
@@ -0,0 +1,4 @@
+#define F(x) x x x
+using S=std::string;S baby_shark_lyrics(){S r,s=" shark";
+for(S i:{"Baby"+s,"Mommy"+s,"Daddy"+s,"Grandma"+s,"Grandpa"+s,S("Let's go hunt")}){
+F(r+=i+','+F(" doo doo")+'\n';)r+=i+"!\n";}return r+"Run away,…";}
