aa92ead6e3c2b01928f82cc9042aa3bf9a9c2f89
Alexander Kobez <alexander.kobez@gmail.com>
2023-08-20 17:40:59 +0300

    Create Predict your age!.cpp

---
+++ 7 kyu/Predict your age!.cpp
@@ -0,0 +1,4 @@
+#include <cmath>
+int predictAge( int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8 ) {
+  return sqrt( a1*a1 + a2*a2 + a3*a3 + a4*a4 + a5*a5 + a6*a6 + a7*a7 + a8*a8 ) / 2;
+}
