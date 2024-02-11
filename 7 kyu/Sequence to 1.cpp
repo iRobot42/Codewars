36cd5dcf63bb3299f36095890a57380b7c3c6282
Alexander Kobez <alexander.kobez@gmail.com>
2024-02-11 18:00:04 +0300

    Update Sequence to 1.cpp

--- 7 kyu/Sequence to 1.cpp
+++ 7 kyu/Sequence to 1.cpp
@@ -1,6 +1,6 @@
 #include <numeric>
 
-#define VARIANT 3
+#define VARIANT 1
 #define NUMBER n > 0 ? n-- : n++
 #define RIGHT v.begin(), v.end()
 #define LEFT v.rbegin(), v.rend()
@@ -8,7 +8,7 @@
 std::vector<int> seqToOne( int n ) {
   std::vector< int > v( n > 0 ? n : 2 - n );
 #if VARIANT == 1
-  for ( size_t i{}; i < v.size(); ++i ) v[ i ] = NUMBER;
+  for ( int& i : v ) i = NUMBER;
 #elif VARIANT == 2
   n > 0 ? iota( LEFT, 1 ) : iota( RIGHT, n );
 #elif VARIANT == 3
