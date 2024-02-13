d259bbca1018f84886b2e7cac3341f01dbe27e46
Alexander Kobez <alexander.kobez@gmail.com>
2024-02-13 21:57:13 +0300

    Update Find ingredients for making glass.cpp

--- 7 kyu/Find ingredients for making glass.cpp
+++ 7 kyu/Find ingredients for making glass.cpp
@@ -1,9 +1,6 @@
-#include <iomanip>
+#include <fmt/format.h>
 std::string createIngredients( double sn = 0, double sw = 0, double ch = 0 ) {
   if ( sn < 0 || sw < 0 || ch < 0 ) return {};
-  double x{ ch ? ch : sw ? sw / 36 : sn ? sn / 12 : 5 };
-  std::ostringstream oss;
-  oss << std::fixed << std::setprecision( 2 )
-    << '[' << x * 12 << ';' << x * 36 << ';' << x << ']';
-  return oss.str();
+  const double x{ ch ? ch : sw ? sw / 36 : sn ? sn / 12 : 5 };
+  return fmt::format( "[{:.2f};{:.2f};{:.2f}]", x * 12, x * 36, x );
 }
