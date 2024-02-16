198fae9458fd0f1bdb7b7a2cc376e8a88aaacc35
Alexander Kobez <alexander.kobez@gmail.com>
2024-02-16 21:40:52 +0300

    Update Get Planet Name By ID.cpp

--- 8 kyu /Get Planet Name By ID.cpp
+++ 8 kyu /Get Planet Name By ID.cpp
@@ -1,4 +1,3 @@
 std::string get_planet_name( int id ) {
-  return std::array{ "Mercury", "Venus", "Earth", "Mars",
-    "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto" }[ --id ];
+  return std::array{ "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune" }[ --id ];
 }
