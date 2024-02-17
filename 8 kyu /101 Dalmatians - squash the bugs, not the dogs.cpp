4488ae9ad0345a06bc8b14fa4cb245e16e3e003a
Alexander Kobez <alexander.kobez@gmail.com>
2024-02-17 16:27:08 +0300

    Update 101 Dalmatians - squash the bugs, not the dogs.cpp

--- 8 kyu /101 Dalmatians - squash the bugs, not the dogs.cpp
+++ 8 kyu /101 Dalmatians - squash the bugs, not the dogs.cpp
@@ -1,6 +1,6 @@
-std::string howManyDalmatians( int number ) {
-  return number <=  10 ? "Hardly any"
-       : number <=  50 ? "More than a handful!"
-       : number == 101 ? "101 DALMATIONS!!!"
-                       : "Woah that's a lot of dogs!";
+std::string howManyDalmatians( int dogs ) {
+  return dogs <= 10 ? "Hardly any"
+       : dogs <= 50 ? "More than a handful!"
+       : dogs == 101 ? "101 DALMATIONS!!!"
+       : "Woah that's a lot of dogs!";
 }
