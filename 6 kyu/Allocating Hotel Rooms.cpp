f417d8d6f7c5f26a28d5d5feb4b06d7c071324da
Alexander Kobez <alexander.kobez@gmail.com>
2024-06-09 00:01:39 +0300

    Create Allocating Hotel Rooms.cpp

---
+++ 6 kyu/Allocating Hotel Rooms.cpp
@@ -0,0 +1,12 @@
+std::vector<int> allocateRooms( std::vector< std::vector< int >>& customers ) {
+  sort( customers.begin(), customers.end() );
+  std::vector< int > rooms, hotel( customers.size() );
+  for ( const auto& customer : customers )
+    for ( size_t room{}; room < hotel.size(); ++room )
+      if ( hotel[ room ] < customer.front() ) {
+        hotel[ room ] = customer.back();
+        rooms.push_back( ++room );
+        break;
+      }
+  return rooms;
+}
