b631e89d33125867f6ace116accf5c303a263ba6
Alexander Kobez <alexander.kobez@gmail.com>
2024-07-07 06:24:11 +0300

    Update Print number with character.cpp

--- 6 kyu/Print number with character.cpp
+++ 6 kyu/Print number with character.cpp
@@ -1,27 +1,19 @@
 #include <fmt/format.h>
 std::string print_number( unsigned num, char ch ) {
-  const std::string
-    number{ fmt::format( "{:05}", num ) },
-    ssolid{ std::string( 40, ch ) },
-    sempty{ ch + std::string( 38, ' ' ) + ch + '\n' };
-  const std::vector< std::vector< std::string >> numbers{
-    { " .... ", "..  ..", "..  ..", "..  ..", "..  ..", " .... " },
-    { "  ..  ", " ...  ", ". ..  ", "  ..  ", "  ..  ", "......" },
-    { " .... ", "..  ..", "   .. ", "  ..  ", " ..   ", "......" },
-    { " .... ", "..  ..", "   .. ", "   .. ", "..  ..", " .... " },
-    { "..  ..", "..  ..", "..  ..", " .....", "    ..", "    .." },
-    { "......", "..    ", "..... ", "    ..", "    ..", "..... " },
-    { "   .. ", "  ..  ", " .... ", "..  ..", "..  ..", " .... " },
-    { "......", "..  ..", "   .. ", "  ..  ", " ..   ", " ..   " },
-    { " .... ", "..  ..", " .... ", " .... ", "..  ..", " .... " },
-    { " .... ", "..  ..", "..  ..", " .... ", "  ..  ", " ..   " }};
-  std::string result{ ssolid + '\n' + sempty };
+  const std::string number{ fmt::format( "{:05}", num ) }, numbers{
+    " .... ..  ....  ....  ....  .. ....   ..   ...  . ..    ..    ..  ......"
+    " .... ..  ..   ..   ..   ..   ...... .... ..  ..   ..    .. ..  .. .... "
+    "..  ....  ....  .. .....    ..    ..........    .....     ..    ....... "
+    "   ..   ..   .... ..  ....  .. .... ........  ..   ..   ..   ..    ..   "
+    " .... ..  .. ....  .... ..  .. ....  .... ..  ....  .. ....   ..   ..   " },
+    out{ std::string( 40, ch ) }, inn{ ch + std::string( 38, ' ' ) + ch + '\n' };
+  std::string result{ out + '\n' + inn };
   for ( size_t i{}; i < 6; ++i ) {
     result += ".  ";
     for ( const char c : number )
-      result += numbers[ c-'0' ][ i ] + ' ';
+      result += numbers.substr(( c-'0' ) * 36 + i * 6, 6 ) + ' ';
     result += " .\n";
   }
   replace( result.begin(), result.end(), '.', ch );
-  return result + sempty + ssolid;
+  return result + inn + out;
 }
