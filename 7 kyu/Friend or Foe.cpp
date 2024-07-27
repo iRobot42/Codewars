c80a879b2140569e8c4d68c753c00afbae0413f5
Alexander Kobez <alexander.kobez@gmail.com>
2024-07-27 08:49:04 +0300

    Create Friend or Foe.cpp

---
+++ 7 kyu/Friend or Foe.cpp
@@ -0,0 +1,7 @@
+using Names_t = std::vector< std::string >;
+Names_t friendOrFoe( const Names_t& input ) {
+  Names_t friends;
+  for ( const auto& name : input )
+    if ( name.size() == 4 ) friends.push_back( name );
+  return friends.empty() ? Names_t{{}} : friends;
+}
