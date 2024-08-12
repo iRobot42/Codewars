3cd36fa30c7988985bc22681786bf71096f0d56d
Alexander Kobez <alexander.kobez@gmail.com>
2024-08-12 11:00:02 +0300

    Update Friend or Foe.cpp

--- 7 kyu/Friend or Foe.cpp
+++ 7 kyu/Friend or Foe.cpp
@@ -3,5 +3,5 @@ Names_t friendOrFoe( const Names_t& input ) {
   Names_t friends;
   for ( const auto& name : input )
     if ( name.size() == 4 ) friends.push_back( name );
-  return friends.empty() ? Names_t{{}} : friends;
+  return friends;
 }
