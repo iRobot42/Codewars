e851ceb8d5ab79afeee03e91b79c27b4d0b8080d
Alexander Kobez <alexander.kobez@gmail.com>
2024-09-22 07:58:07 +0300

    Update The Office I - Outed.cpp

--- 7 kyu/The Office I - Outed.cpp
+++ 7 kyu/The Office I - Outed.cpp
@@ -1,6 +1,6 @@
 #include <numeric>
 std::string outed( const std::map< std::string, int >& meet, const std::string& boss ) {
-  return ( std::accumulate( meet.cbegin(), meet.cend(), 0., []( int acc, const auto& p ) {
-    return acc + p.second; }) + meet.at( boss )) / meet.size() <= 5
+  return std::accumulate( meet.cbegin(), meet.cend(), double( meet.at( boss )),
+    []( auto acc, const auto& p ) { return acc + p.second; }) / meet.size() <= 5
       ? "Get Out Now!" : "Nice Work Champ!";
 }
