87cefd412b9115011f998801447a126f0041d4c7
Alexander Kobez <alexander.kobez@gmail.com>
2024-10-01 05:18:29 +0300

    Create Combinations Produced by n Coin Flips.cpp

---
+++ 6 kyu/Combinations Produced by n Coin Flips.cpp
@@ -0,0 +1,7 @@
+#include <bitset>
+std::vector<std::string> coin_flips( int flips ) {
+  std::vector< std::string > combinations( 1 << flips );
+  for ( size_t i{}; i < combinations.size(); ++i )
+    combinations[ i ] = std::bitset< 10 >( i ).to_string( 'H','T' ).substr( 10 - flips );
+  return combinations;
+}
