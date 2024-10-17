b4b7048e1079a9f01dcc25fdcd117c1cecedced2
Alexander Kobez <alexander.kobez@gmail.com>
2024-10-18 02:51:11 +0300

    Update Spiral Column Addition.cpp

--- 6 kyu/Spiral Column Addition.cpp
+++ 6 kyu/Spiral Column Addition.cpp
@@ -1,4 +1,4 @@
-unsigned long long spiral_column( unsigned int n, unsigned int c ) {
+unsigned long long spiral_column( unsigned n, unsigned c ) {
   unsigned long long s{};
   while ( c < n ) s += ( 2ull * n - 1 ) * ( n - 1 ) + c, c = n-- - c;
   return n * ( 3ull * n - 1 ) / 2 + s;
