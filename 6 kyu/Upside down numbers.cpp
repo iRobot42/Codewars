5a7f2211b4b0d1413c6f6328ff9a8b1a3a63a9fe
Alexander Kobez <alexander.kobez@gmail.com>
2024-10-23 02:04:22 +0300

    Create Upside down numbers.cpp

---
+++ 6 kyu/Upside down numbers.cpp
@@ -0,0 +1,15 @@
+int solve( int x, int y ) {
+  constexpr auto rotated{ []( const int num ) {
+    int rot{}, tmp{ num }, d;
+    do switch ( rot *= 10, d = tmp % 10 ) {
+      case 0: case 1: case 8: rot += d; break;
+      case 6: rot += 9; break;
+      case 9: rot += 6; break;
+      default: return false;
+    } while ( tmp /= 10 );
+    return rot == num; }};
+  int count{};
+  do if ( rotated( x )) ++count;
+  while ( ++x < y );
+  return count;
+}
