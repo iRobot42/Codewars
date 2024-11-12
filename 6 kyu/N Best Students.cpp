338c92ffd6f90b508c7a8e4f40c4b3c3fb3c6e7a
Alexander Kobez <alexander.kobez@gmail.com>
2024-11-12 12:18:55 +0300

    Update N Best Students.cpp

--- 6 kyu/N Best Students.cpp
+++ 6 kyu/N Best Students.cpp
@@ -1,10 +1,15 @@
 #include <range/v3/numeric/accumulate.hpp>
+#include <range/v3/algorithm/partial_sort.hpp>
+
 using Names_t = std::vector< std::string >;
 Names_t nBestStudents( Names_t students, std::vector< std::vector< float >> grades, int n ) {
-  std::map< float, std::string > avg;
-  for ( size_t i{}; i < students.size(); ++i )
-    avg[ ranges::accumulate( grades[ i ], 0. ) / grades[ i ].size() ] = students[ i ];
-  Names_t best; best.reserve( n );
-  for ( auto it{ avg.crbegin() }; n--; ++it ) best.push_back(( *it ).second );
-  return best;
+  std::vector< std::pair< float, std::string >> vs( students.size() );
+  for ( size_t i{}; i < vs.size(); ++i )
+    vs[ i ] = { ranges::accumulate( grades[ i ], 0. ), students[ i ] };
+  ranges::partial_sort( vs, vs.begin() + n, std::greater() );
+  for ( int i{}; i < n; ++i ) students[ i ] = vs[ i ].second;
+  return students.resize( n ), students;
 }
+
+// NB: in this kata, calculation of the average is redundant,
+// since students will all have the same number of grades.
