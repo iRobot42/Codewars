51d9dd3dbdde15445945338b9f7fd47e8f896c20
Alexander Kobez <alexander.kobez@gmail.com>
2024-12-01 08:12:42 +0300

    Update The Deaf Rats of Hamelin.cpp

--- 6 kyu/The Deaf Rats of Hamelin.cpp
+++ 6 kyu/The Deaf Rats of Hamelin.cpp
@@ -1,5 +1,5 @@
 #include <range/v3/algorithm/count_if.hpp>
-int countDeafRats( std::string_view town, bool f = true ) {
-  return ranges::count_if( town, [ &f ]( const char c ) {
-    return c != ' ' && ( f = !f ) && c == '~'; });
+int countDeafRats( std::string_view town ) {
+  return ranges::count_if( town, [ f{ 1 } ]( const char c ) mutable {
+    return c != ' ' && ( f = !f ) && c == '~'; }); // lazy evaluation
 }
