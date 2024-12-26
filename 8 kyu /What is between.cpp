fb3d94f681939049e7d089a3d7bcdb5abf68a8dc
Alexander Kobez <alexander.kobez@gmail.com>
2024-12-26 04:38:45 +0300

    Update What is between.cpp

--- 8 kyu /What is between.cpp
+++ 8 kyu /What is between.cpp
@@ -1,5 +1,5 @@
+#include <range/v3/numeric/iota.hpp>
 std::vector<int> between( int start, int end ) {
-  std::vector< int > v;
-  while ( start <= end ) v.push_back( start++ );
-  return v;
+  std::vector< int > v( end - start + 1 );
+  return ranges::iota( v, start ), v;
 }
